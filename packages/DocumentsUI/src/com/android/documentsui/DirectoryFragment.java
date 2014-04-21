1b35f90151429757060b42e75c9295d32fff58b5
Felix Ableitner <me@nutomic.com>
2014-04-21 22:18:34 +0200

    Replaced custom strings with Android defaults
    PS2: also replaced menu_cut/copy/paste with strings from android.R.string
    
    Change-Id: I7695b283faa66c6997fad372b31de2c678d222d0
    
    Conflicts:
    	packages/DocumentsUI/res/menu/activity.xml
    	packages/DocumentsUI/res/menu/mode_directory.xml
    	packages/DocumentsUI/res/values/cr_strings.xml
    	packages/DocumentsUI/src/com/android/documentsui/DirectoryFragment.java

--- packages/DocumentsUI/src/com/android/documentsui/DirectoryFragment.java
+++ packages/DocumentsUI/src/com/android/documentsui/DirectoryFragment.java
@@ -585,7 +585,33 @@ public class DirectoryFragment extends Fragment {
         startActivity(intent);
     }
 
-    private void onDeleteDocuments(List<DocumentInfo> docs) {
+    private void onDeleteDocuments(final List<DocumentInfo> docs) {
+        final Context context = getActivity();
+        final ContentResolver resolver = context.getContentResolver();
+        final Resources resources = context.getResources();
+
+        // Open a confirmation dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                new DeleteFilesTask(docs.toArray(new DocumentInfo[0])).executeOnExecutor(getCurrentExecutor());
+            }
+        });
+        builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog, ignore actions
+            }
+        });
+
+        builder.setTitle(R.string.dialog_delete_confirm_title)
+            .setMessage(resources.getQuantityString(R.plurals.dialog_delete_confirm_message, docs.size(), docs.size()));
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private boolean onDeleteDocumentsImpl(final List<DocumentInfo> docs) {
         final Context context = getActivity();
         final ContentResolver resolver = context.getContentResolver();
 
