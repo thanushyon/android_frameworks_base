d9e9247f4fdd902d58c9963f6b54bc326eee362e
westcripp <korukaltan@gmail.com>
2014-11-29 21:12:52 +0200

    Base: Network speed indicator
    port from omnirom kitkat
    
    Change-Id: I5efdaf1e5bd5f1e93fb9cc03681a089228f27757
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3280,6 +3280,15 @@ public final class Settings {
          * @hide
          */
         public static final String KEY_MENU_ACTION = "key_menu_action";
+        /**
+         * Network traffic indicator, goes from least to greatest significant bitwise
+         * 0 = Display up-stream traffic if set
+         * 1 = Display down-stream traffic if set
+         * 2 = Show as Byte/s if set
+         * 16-31 = Refresh interval(ms)  min: 250  max: 32750  default: 1000
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state"; 
 
         /**
          * Action to perform when the menu key is long-pressed.
