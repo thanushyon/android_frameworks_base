bbf4fbe170883fe5a07bc67101be41788638d849
kenzyun_chen <kenzyun_chen@htc.com>
2014-12-01 15:00:13 +0800

    Fix home key behavior for multiple CarHome app
    Symptom: press home key can't go back to CarHome app
    Root Cause: Car app picker (after installing multiple car app) caused createHomeDockIntent() returning null
    Solution: return intent for Car app picker case
    
    Change-Id: Ibd04e87e7e1f7da2bc4b94f7f22c470eab295292

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -6916,7 +6916,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             return intent;
         }
 
-        return null;
+        return intent;
     }
 
     void startDockOrHome() {
